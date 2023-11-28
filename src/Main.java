e69c8e322b3996b2f8b337e8105ef06cd50252b2
Janmesh <janmesh5900@gmail.com>
2023-11-28 21:19:25 +0530

    Greeting

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+//        System.out.println("Hello world!");
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        String name = input.next();
+        String person = greeting(name);
+        System.out.println(person);
+    }
+
+    static String greeting(String name) {
+        String message = "Hello " + name;
+        return message;
+//        System.out.println("kaise ho tum?" + greet);
     }
 }
